50d1e2706941b8d26449465802c771a061c0e145
collenhornaman12-hue <collenhornaman12@gmail.com>
2026-05-15 13:42:48 -0400

    fix: add forwardRef to Badge, explicit displayName on Progress

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -17,6 +17,6 @@ const Progress = React.forwardRef<
     />
   </ProgressPrimitive.Root>
 ))
-Progress.displayName = ProgressPrimitive.Root.displayName
+Progress.displayName = 'Progress'
 
 export { Progress }
